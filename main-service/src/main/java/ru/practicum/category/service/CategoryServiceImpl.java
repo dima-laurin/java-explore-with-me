4eaa992b94741adafd9cdf6452043412556febfd
Dmitry <dima-laurin@yandex.ru>
2026-08-16 16:17:32 +0300

    fix

--- main-service/src/main/java/ru/practicum/category/service/CategoryServiceImpl.java
+++ main-service/src/main/java/ru/practicum/category/service/CategoryServiceImpl.java
@@ -13,7 +13,6 @@ import ru.practicum.event.repository.EventRepository;
 import ru.practicum.exception.ConflictException;
 
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import java.util.List;
